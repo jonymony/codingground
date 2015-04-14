54d40c4cb1ac2985315a9f4d5a27a038f199776e
jonymony <jony.jonymony.mony8@gmail.com>
2015-04-14 17:18:25 +0530

    Created by Coding Ground Client

---
+++ ghjt/spectrumproc.h
@@ -0,0 +1,20 @@
+// SpectrumProc.h: interface for the CSpectrumProc class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_SPECTRUMPROC_H__1FFE7FC1_EA30_11D5_97A5_00C04FA11197__INCLUDED_)
+#define AFX_SPECTRUMPROC_H__1FFE7FC1_EA30_11D5_97A5_00C04FA11197__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CSpectrumProc  
+{
+public:
+	CSpectrumProc();
+	virtual ~CSpectrumProc();
+
+};
+
+#endif // !defined(AFX_SPECTRUMPROC_H__1FFE7FC1_EA30_11D5_97A5_00C04FA11197__INCLUDED_)
